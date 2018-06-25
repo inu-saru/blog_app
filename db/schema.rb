bcf392b89f09942f93b0034b3909ff1843b20e3b
EC2 Default User <ec2-user@ip-172-31-9-80.us-east-2.compute.internal>
2018-06-25 18:42:42 +0000

    validaton user and install TEST tool

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180625111543) do
+ActiveRecord::Schema.define(version: 20180625182936) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
