4736dbb77f6f1f70cc39c3b3ec5749eebef67b2c
EC2 Default User <ec2-user@ip-172-31-9-80.us-east-2.compute.internal>
2018-06-26 04:06:26 +0000

    Implement advanced login (cookie)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180625184631) do
+ActiveRecord::Schema.define(version: 20180626034825) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180625184631) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
