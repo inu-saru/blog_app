c63a4068798b67bf086aa1226b8298c68d6c244c
EC2 Default User <ec2-user@ip-172-31-9-80.us-east-2.compute.internal>
2018-06-27 04:02:54 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root      'posts#index'
   resources :posts
   resources :users
@@ -6,4 +10,5 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
